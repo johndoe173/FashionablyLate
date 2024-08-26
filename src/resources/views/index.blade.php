3ca55362d0f0e89195fdfd1857a9bef13b1eb371
johndoe <hotaru3712@yahoo.co.jp>
2024-08-27 01:39:29 +0900

    コード編集

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -8,7 +8,7 @@
 
 <div class="contact-form__content">
   <div class="contact-form__heading">
-    <h2>お問い合わせ</h2>
+    <h2>Contact</h2>
   </div>
   <form class="form" action="contacts/confirm" method="post">
     @csrf
